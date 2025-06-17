eb1fe3dd8d1c0500e59cb35a3385f29c5fe52b1d
Ilia Proskurin <pilailiapro@gmail.com>
2025-06-17 07:47:53 +0300

    profession qualities

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,39 +1,75 @@
 import { data } from "./data"; //  используем начальные данные
-
 import "./Table.css";
 
 function Table() {
-  function HandleClick(event) {
-    const button = event.target.closest("button");
-    if (button) {
-      const p = button.closest("tr");
-      const result = data.find((item) => item._id === p.id);
-      console.log(result);
+  const columns = {
+    name: {
+      path: "name",
+      name: "Имя",
+      component: (item) => {
+        return <p>{item.name}</p>;
+      },
+    },
+    age: {
+      path: "age",
+      name: "Возраст",
+    },
+    qualities: {
+      path: "qualities",
+      name: "Качества",
+      component: (item, column) => {
+        return item[column].map((item) => (
+          <p className={"itemQualities " + item.color}>{item.name}</p>
+        ));
+      },
+    },
+    profession: {
+      path: "profession",
+      name: "Профессия",
+    },
+    delete: {
+      name: "Удалить",
+      component: (item) => {
+        return <button onClick={() => console.log(item)}>Delete</button>;
+      },
+    },
+  };
+
+  const renderColumn = (item, column) => {
+    const component = columns[column].component;
+    const path = columns[column].path;
+    if (component && typeof component === "function") {
+      return component(item, column);
+    } else if (path === "age") {
+      return item[column];
+    } else if (path === "profession") {
+      return item[column].name;
     }
-  }
+  };
+
   return (
     <>
       <h1>Table</h1>
       <table>
         <thead>
           <tr>
-            <td> имя </td>
-            <td> возраст </td>
-            <td> удалить </td>
+            {Object.keys(columns).map((column) => (
+              <th key={column}>{columns[column].name}</th>
+            ))}
           </tr>
         </thead>
-        <tbody onClick={HandleClick}>
-          {data.map((item) => (
-            <tr id={item._id} key={item._id}>
-              <td>{item.name}</td>
-              <td>{item.age}</td>
-              <td>
-                <button>удалить</button>
-              </td>
+        <tbody>
+          {data.users.map((item) => (
+            <tr key={item._id}>
+              {Object.keys(columns).map((column) => {
+                return <td key={column}>{renderColumn(item, column)}</td>;
+              })}
             </tr>
           ))}
         </tbody>
       </table>
+
+      {/* делаем динамический рендер таблицы */}
     </>
   );
 }
--- src/data/index.js
+++ src/data/index.js
@@ -1,64 +1,127 @@
-export const data = [
-	{
-		_id: '67rdca3eeb7f6fgeed471815',
-		name: 'Джон Дориан',
-		age: 36,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471816',
-		name: 'Кокс',
-		age: 15,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471817',
-		name: 'Боб Келсо',
-		age: 47,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471818',
-		name: 'Рэйчел Грин',
-		age: 48,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471819',
-		name: 'Шелдон Купер',
-		age: 37,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471820',
-		name: 'Леонард Хофстедтер',
-		age: 47,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471821',
-		name: 'Говард Воловиц',
-		age: 72,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471822',
-		name: 'Никола Тесла',
+import { professionsObject as professions } from "./profession";
 
-		age: 72,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471823',
-		name: 'Моника Геллер',
+const qualities = {
+  tedious: {
+    _id: "67rdca3eeb7f6fgeed471198",
+    name: "Нудила",
+    color: "primary",
+  },
+  strange: {
+    _id: "67rdca3eeb7f6fgeed471100",
+    name: "Странный",
+    color: "secondary",
+  },
+  buller: {
+    _id: "67rdca3eeb7f6fgeed4711012",
+    name: "Троль",
+    color: "success",
+  },
+  alcoholic: {
+    _id: "67rdca3eeb7f6fgeed471101",
+    name: "Алкоголик",
+    color: "danger",
+  },
+  handsome: {
+    _id: "67rdca3eeb7f6fgeed471102",
+    name: "Красавчик",
+    color: "info",
+  },
+  uncertain: {
+    _id: "67rdca3eeb7f6fgeed471103",
+    name: "Неуверенный",
+    color: "dark",
+  },
+};
 
-		age: 17,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed471824',
-		name: 'Рататуй',
-		age: 17,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed47181f',
-		name: 'Джоуи Триббиани',
-		age: 434,
-	},
-	{
-		_id: '67rdca3eeb7f6fgeed47181r',
-		name: 'Анджелина Джоли',
-		age: 34,
-	},
-]
+const users = [
+  {
+    _id: "67rdca3eeb7f6fgeed471815",
+    name: "Джон Дориан",
+    profession: professions.doctor,
+    qualities: [qualities.tedious, qualities.uncertain, qualities.strange],
+    age: 45,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471816",
+    name: "Кокс",
+    profession: professions.doctor,
+    qualities: [qualities.buller, qualities.handsome, qualities.alcoholic],
+    age: 45,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471817",
+    name: "Боб Келсо",
+    profession: professions.doctor,
+    qualities: [qualities.buller],
+    age: 44,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471818",
+    name: "Рэйчел Грин",
+    profession: professions.waiter,
+    qualities: [qualities.uncertain],
+    age: 35,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471819",
+    name: "Шелдон Купер",
+    profession: professions.physics,
+    qualities: [qualities.strange, qualities.tedious],
+    age: 24,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471820",
+    name: "Леонард Хофстедтер",
+    profession: professions.physics,
+    qualities: [qualities.strange, qualities.uncertain],
+    age: 28,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471821",
+    name: "Говард Воловиц",
+    profession: professions.engineer,
+    qualities: [qualities.strange, qualities.tedious],
+    age: 23,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471822",
+    name: "Никола Тесла",
+    profession: professions.engineer,
+    qualities: [qualities.handsome],
+    age: 45,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471823",
+    name: "Моника Геллер",
+    profession: professions.cook,
+    qualities: [qualities.strange, qualities.uncertain],
+    age: 32,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed471824",
+    name: "Рататуй",
+    profession: professions.cook,
+    qualities: [qualities.handsome, qualities.buller],
+    age: 32,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed47181f",
+    name: "Джоуи Триббиани",
+    profession: professions.actor,
+    qualities: [qualities.uncertain, qualities.strange],
+    age: 22,
+  },
+  {
+    _id: "67rdca3eeb7f6fgeed47181r",
+    name: "Брэд Питт",
+    profession: professions.actor,
+    qualities: [qualities.handsome],
+    age: 45,
+  },
+];
+
+export const data = {
+  users,
+  qualities,
+  professions,
+};
